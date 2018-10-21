3570726a7ba7df6057f8a520e3eacb4ecc5ac0ba
BenMirtchouk <benya.usa@gmail.com>
2018-10-21 15:36:12 -0400

    proper extraction with cigar / more general / better format

--- code/check.py
+++ code/check.py
@@ -1,46 +1,52 @@
 import os
 
+snv = [820, 842, 848, 854, 872, 875, 878, 884, 887]
+cor = ['A', 'T', 'A', 'A', 'T', 'G', 'G', 'A', 'G']
+    
 files = os.listdir("alignments")
 for name in files:
     f = open("alignments/" + name, "r")
 
-    snv = [842, 848, 854, 872, 875, 878, 884, 887]
-    cor = ['T', 'A', 'A', 'T', 'G', 'G', 'A', 'G']
-    for i in range(len(snv)):
-        snv[i] -= 775
-
     correct = 0
     reads = 0
+    offset = int(name.split("_")[4])
 
     if "pairwise" in name:
-        score = f.readline()
-        while score:
+        seq = f.readline()
+        while seq:
             reads += 1
-            seq = f.readline()[:-1]
+            seq = seq[:-1]
             read = f.readline()[:-1]
-            n = f.readline()
-            n = f.readline()
-            score = f.readline()
+            nl = f.readline()
             
             idx = 0
             sp = 0
+            
+            for sp in range(len(snv)): #
+                if snv[sp] - offset - 1 >= 0: #
+                    break #
+            
+            print name,'starting at',sp,snv[sp]
             for i in range(len(seq)):
                 if seq[i] == '-': 
                     continue
                 
-                if sp < len(snv) and idx == snv[sp]:
+                if sp < len(snv) and idx == (snv[sp] - (835 if '834' in name else 801)):
                     if read[i] == cor[sp]:
                         correct += 1
                     
                     sp += 1
                 
-                idx += 1    
+                idx += 1
+            
+            seq = f.readline()    
     else:
-        seq = f.readline().split()[1]
+        
+        seq = f.readline().split()[1][:-1]
         l = f.readline()
         while l:
             reads += 1
-            read = l.split()[1]
+            read = l.split()[1][:-1]
             l = f.readline()
             
             idx = 0
@@ -49,7 +55,7 @@ for name in files:
                 if seq[i] == '-': 
                     continue
                 
-                if sp < len(snv) and idx == snv[sp]:
+                if sp < len(snv) and idx == (snv[sp] - (835 if '834' in name else 801)):
                     if read[i] == cor[sp]:
                         correct += 1
                     
--- code/get_reads.py
+++ code/get_reads.py
@@ -1,29 +1,79 @@
 def intersect(a, b):
     return (max(a[0], b[0]), min(a[1], b[1]))
 
+def get_int(i, s):
+    ret = 0
+    while i < len(s) and s[i].isdigit():
+        ret = ret*10 + int(s[i])
+        i += 1
+    return ret, i
+
 rng = (834, 895)
 
-o = open("data/61_to_78__"+str(rng[0])+"_to_"+str(rng[1])+".txt", "w")
-f = open("data/MSSA_78_1720493_1789360.fa", "r")
+o = open('data/61_to_78__' + str(rng[0]) + '_to_' + str(rng[1]) + '.txt', 'w')
+f = open('data/MSSA_78_1720493_1789360.fa', 'r')
 
 f.readline()
-o.write(">seq\n" + "".join(f.readlines()).replace("\n","")[rng[0] - 60 : rng[1] + 60 + 1] + "\n")
+reference = ''.join(f.readlines()).replace('\n','')[rng[0]: rng[1] + 1]
+print reference
+o.write('>seq\n' + reference + '\n')
 f.close()
 
-f = open("data/61_to_78_parsed.txt", "r")
-#name, start, seq
-l = f.readline()
+f = open('data/61_to_78.txt', 'r')
 
+l = f.readline()
 while l:
     spl = l.split()
-    st = int(spl[1])
-    ln = len(spl[2])
-
-    inter = intersect((rng[0] - 10, rng[1] + 10), (st, st+ln-1))
-    if inter[0] <= inter[1] and inter[1] - inter[0] + 1 > 30:
-        o.write(">" + spl[0].split("/")[-1] + "\n" + spl[2][inter[0] - st : inter[1] - st + 1] + "\n")
-    
     l = f.readline()
+    
+    # BAM format
+    name = spl[0]
+    pos = spl[3]
+    cigar = spl[5]
+    seq = spl[9]
+    
+    pos = int(pos) - 1 # 0 index
+    
+    if pos > rng[0]:
+        continue
+    
+    seq_idx = pos
+    ref_idx = pos
+    cigar_idx = 0
+    
+    aligned = ''
+    
+    while cigar_idx < len(cigar):
+        q, cigar_idx = get_int(cigar_idx, cigar)
+        ch = cigar[cigar_idx]
+        
+        # print q, ch
+        
+        if ch == 'M':
+            inter = intersect((ref_idx, ref_idx + q - 1), rng)
+            if inter[0] <= inter[1]:
+                aligned += seq[seq_idx + (inter[0] - ref_idx) : seq_idx + (inter[1] - ref_idx) + 1]
+            
+            seq_idx += q
+            ref_idx += q
+        elif ch == 'D':
+            inter = intersect((ref_idx, ref_idx + q - 1), rng)
+            if inter[0] <= inter[1]:
+                aligned += '-' * (inter[1] - inter[0] + 1)
+            
+            ref_idx += q
+        elif ch == 'I':
+            seq_idx += q
+        else:
+            print 'ohno', ch
+            exit(-1)
+        
+        cigar_idx += 1
+    
+    if len(aligned) >= (rng[1] - rng[0] + 1) // 2:
+        o.write('>' + name.split('/')[-1] + '\n')
+        o.write(aligned + '\n')
+        print aligned
 
 f.close()
 o.close()
