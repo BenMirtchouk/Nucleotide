789bd4fd9bccac3a01c2b345003479fd9a1c2bdf
Edward Li <edwardyaoli@gmail.com>
2019-05-01 16:02:37 -0400

    better cli argument parsing

--- code/Nucleotide.py
+++ code/Nucleotide.py
@@ -2,22 +2,25 @@ from Graph import *
 from Aligner import *
 import sys
 from math import log, ceil
+import argparse
 
-if len(sys.argv) < 3:
-    print 'usage: ./test_real_reads.py {number of reads} {file} [-v]'
-    exit(0)
+ap = argparse.ArgumentParser()
+ap.add_argument('n_seqs', type=int, help="number of reads")
+ap.add_argument('file', help="path to file")
+ap.add_argument('--verbose', '-v', action='store_true', help="toggle verbosity")
+args = vars(ap.parse_args())
 
 seqs = []
-f = open(sys.argv[2],"r")
+f = open(args['file'], "r")
 for i in range(100):
     name,seq = f.readline(),f.readline()
     seqs.append(Graph(seq, name[1:]))
-  
-verbose = len(sys.argv) > 3 and (sys.argv[3] == '-v' or sys.argv[3] == '--verbose')
-  
+
+verbose = args['verbose']
+
 data = ''
 
-n_seqs = int(sys.argv[1])
+n_seqs = args['n_seqs']
 n_seqs = min(n_seqs, len(seqs))
 
 n = int(ceil(log(n_seqs,2)))
@@ -26,9 +29,9 @@ for i in range(1,n+1):
     for j in range(0, 1<<n, 1<<i):
         if j+(1<<(i-1)) >= n_seqs:
             break
-        
+
         print 'aligning',j,j+(1<<(i-1))
-        
+
         align = Aligner(seqs[j], seqs[ j+(1<<(i-1)) ])
         align.align()
 
